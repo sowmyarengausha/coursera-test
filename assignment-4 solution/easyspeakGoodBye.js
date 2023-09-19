4c8deb21f969cb675c65d68b6c6f53fa6271cf79
sowmyarengausha <sowmyarengausha@gmail.com>
2023-09-19 11:50:15 +0530

    Updated version

--- assignment-4 solution/easyspeakGoodBye.js
+++ assignment-4 solution/easyspeakGoodBye.js
@@ -22,6 +22,8 @@ byeSpeaker.speak=function(name) {
 }
 window. byeSpeaker= byeSpeaker;
 })(window);
+
+window.onload("Bye!!");
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
